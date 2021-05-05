833ba28755df983c6125400f17787cce1ed16839
andrew <andrewkalennyk@gmail.com>
2021-05-05 19:48:46 +0300

    migration + dbal

---
+++ database/migrations/2021_05_05_163731_add_nullable_to_tb_tree.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddNullableToTbTree extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tb_tree', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+            $table->string('picture')->nullable()->change();
+            $table->text('additional_pictures')->nullable()->change();
+            $table->string('seo_title')->nullable()->change();
+            $table->string('seo_description')->nullable()->change();
+            $table->string('seo_keywords')->nullable()->change();
+        });
+
+        DB::statement("ALTER TABLE tb_tree MODIFY is_show_in_menu TINYINT UNSIGNED NULL");
+        DB::statement("ALTER TABLE tb_tree MODIFY is_show_in_footer_menu TINYINT UNSIGNED NULL");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tb_tree', function (Blueprint $table) {
+            //
+        });
+    }
+}
